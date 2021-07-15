9c05530193a1aad407f5c161a6d0768f7489fd71
Virender negi <58510282+viren760@users.noreply.github.com>
2021-07-15 18:58:15 +0530

    Added

---
+++ src/com/Banking/Interest.java
@@ -0,0 +1,17 @@
+package com.Banking;
+
+public class Interest {
+	
+	String loans;
+	int interest;
+	
+	Interest(String loans, int interest){
+		this.loans= loans;
+		this.interest=interest;
+		System.out.println("The interest of "+loans+" is "+interest+"%");
+		System.out.println();
+		System.out.println();
+		System.out.println("     THANKYOU FOR VISITING :)      ");
+	}
+
+}
